a7994bed9546c8fb20e99c4808e0a311a207d046
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2023-10-02 16:52:25 -0500

    basic variables

--- app/src/main/java/com/santiagobulla/desarollomovil/Variables.kt
+++ app/src/main/java/com/santiagobulla/desarollomovil/Variables.kt
@@ -1,6 +1,30 @@
 package com.santiagobulla.desarollomovil
 
-fun main(){
-    print("hello world third attempt")
+fun main() {
+    /*Variables numericas*/
+
+    //numeros enteros Int
+    val numeroEntero: Int = 18
+    //numeros entero muy grandres
+    val enteroGrande: Long = 12345678990
+    //numeros decimales Float deben terminar con la letra f minuscula
+    val decimal: Float = 16.9f
+    //numeros decimales grandes Double
+    val decimalGrande: Double = 3.1416//...
+
+
+    /*variables alfanumericas textos*/
+
+    //Char soporta un caracter y va en comillas simples
+    val caracter1: Char = 'p'
+    val caracter2: Char = '@'
+    val caracter3: Char = '3'
+
+    //textos String con comillas dobles
+    val txt: String = "hola mundo"
+
+    /*variables booleamas*/
+    val boleano1: Boolean = true
+    val boleano2: Boolean = false
 }
 
