b47aca3d6371cf2c7a0dc5ef8cf767f5379eaff6
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2023-10-11 17:53:28 -0500

    learning how to work with constraint layouts -> resposive design

---
+++ app/src/main/java/com/santiagobulla/desarollomovil/firtsApp/ConstrainLayouts.kt
@@ -0,0 +1,12 @@
+package com.santiagobulla.desarollomovil.firtsApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.santiagobulla.desarollomovil.R
+
+class ConstrainLayouts : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constrain_layouts)
+    }
+}
